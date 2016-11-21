f46e02a0718346cf13fe48c832ae39d3cacc432a
aleczander <afigueroa81@ufl.edu>
2016-11-21 18:39:42 -0500

    implemented get by id route

--- server/routers/post.router.js
+++ server/routers/post.router.js
@@ -15,9 +15,35 @@ router.get('/posts', function(req, res){
   });
 });
 
-router.get('/posts/:id', function(req, res){});
+router.get('/posts/:id', function(req, res){
+  Post.find({_id: req.params.id}, function(err, post){
+    if(err){
+      return res.status(500).json({
+        msg:err
+      });
+    }
+    return res.status(200).json({
+      posts:posts
+    });
+  });
+});
 
-router.post('/posts', function(req, res){});
+router.post('/posts', function(req, res){
+  console.log(req.body);
+  var post = new Post(req.body);
+  post.postDate = new Date();
+  post.summary = req.body.body.splice(0,100) + '...';
+  post.save(function(err){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+    return res.status(201).json({
+      msg: 'Success!'
+    });
+  });
+});
 
 router.put('/posts/:id', function(req, res){});
 
